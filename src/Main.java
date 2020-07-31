0b08d56617829be497f0f5ae6fcf423001d24420
Gustavo Rivero <gustavoerivero12@gmail.com>
2020-07-31 15:07:38 -0400

    Se añaden las librerías iniciales (reciclando de otros proyectos)

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 
+import controllers.ControllerMainMenu;
+
 /**
  *
  * @author Gustavo
@@ -9,7 +11,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
--- src/controllers/ControllerMainMenu.java
+++ src/controllers/ControllerMainMenu.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package controllers;
 
 /**
@@ -10,5 +6,7 @@ package controllers;
  * @author Gustavo
  */
 public class ControllerMainMenu {
+    
+    
     
 }
