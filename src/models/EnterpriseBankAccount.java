239317ff54c32068bdda20f03a00dc56ec93c3fb
Gustavo Rivero <gustavoerivero12@gmail.com>
2020-08-16 11:57:22 -0400

    Update: AssociatedAccount with Wallet (models) and UML updates

--- src/models/EnterpriseBankAccount.java
+++ src/models/EnterpriseBankAccount.java
@@ -7,8 +7,6 @@ package models;
  */
 public class EnterpriseBankAccount extends BankAccount{
     
-    private int     walletId;
-    
     private double  amount;
 
     /**
@@ -16,7 +14,6 @@ public class EnterpriseBankAccount extends BankAccount{
      * @param amount Monto de la cuenta.
      * @param id Atributo identificador (Autoincrementable).
      * @param ownerId Atributo identificador del propietario de la cuenta.
-     * @param walletId Atributo identificador de la billetera (de poseer).
      * @param dni Documento Nacional de Identidad de la Empresa.
      * @param name Nombre del propietario asocado a la cuenta.
      * @param bank Banco asociado a la cuenta.
@@ -25,23 +22,14 @@ public class EnterpriseBankAccount extends BankAccount{
      * @param phone Teléfono asociado a la cuenta.
      * @param state Estado (Activo o Inactivo).
      */
-    public EnterpriseBankAccount(double amount, int id, int ownerId, int walletId, 
+    public EnterpriseBankAccount(double amount, int id, int ownerId,
             String dni, String name, String bank, String email, long account, 
             long phone, char state) {
         
         super(id, ownerId, dni, name, bank, email, account, phone, state);
         
-        this.walletId = walletId;
         this.amount = amount;
         
-    }
-
-    public int getWalletId() {
-        return walletId;
-    }
-
-    public void setWalletId(int walletId) {
-        this.walletId = walletId;
     }
     
     public double getAmount() {
